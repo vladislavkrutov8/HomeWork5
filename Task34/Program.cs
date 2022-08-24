1385a3fc9b4a05dfcc1a134597e0ce2ec6d6629a
vladislavkrutov8 <110223646+vladislavkrutov8@users.noreply.github.com>
2022-08-24 20:44:24 +0300

    Add files via upload

---
+++ Task34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int max, int min)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000 );    
+    }        
+return array;
+}
+
+int QuantityPositive(int[] array){
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ ){
+    if (array[i] % 2 == 0)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+
+    }
+    Console.WriteLine("]");
+    }
+  
+    
+int[] arr = CreateArrayRndInt(10, 100, 1000);
+PrintArray(arr);
+int quantity = QuantityPositive(arr);
+Console.Write($"Количество четных чисел = {quantity}");
+
---
+++ Task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int max, int min)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(-10, 100);    
+    }        
+return array;
+}
+
+int QuantityPositive(int[] array){
+    int sum = 0;
+    for (int i = 0; i<array.Length; i = i + 2){
+//     if (array[i] % 2 == 0)
+//     {
+//       quantity++;
+//     }
+//   }
+sum = sum+array[i]; 
+    }
+  return sum;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+
+    }
+    Console.WriteLine("]");
+    }
+  
+    
+int[] arr = CreateArrayRndInt(10, -10, 100);
+PrintArray(arr);
+int sum = QuantityPositive(arr);
+Console.Write($"Сумма элементов на нечетных индексах = {sum}");
